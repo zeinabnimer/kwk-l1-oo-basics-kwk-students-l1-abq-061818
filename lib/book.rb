f0fb95cad60843a45376d304d15001da38c750a5
zeinabnimer <www.zeinab6@gmail.com>
2018-06-21 22:07:58 +0000

    automatically backed up by learn

--- lib/book.rb
+++ lib/book.rb
@@ -18,6 +18,22 @@ def author
   @author
 end
 
+def page_count= (num)
+  @page_count = num
 end
 
-Book.new("And Then There Were None")
+def page_count
+  @page_count
+end
+
+def genre= (genre)
+  @genre = genre
+end
+
+def genre
+  @genre
+end
+
+end
+
+#Book.new("And Then There Were None")
